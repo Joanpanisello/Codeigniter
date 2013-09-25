5a95f7384e9bfe6f7b3b382b2c949b135ef1b906
root <root@JPC.(none)>
2013-09-25 20:53:02 +0200

    codeigniten

---
+++ application/controllers/Usuaris.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usuaris extends CI_Controller {
+
+
+
+	public function Crear()
+	{
+		$this->load->view('welcome_message');
+	}
+	
+	public function Modificar()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	public function Eliminar()
+	{
+		$this->load->view('welcome_message');
+	}
+	
+}
+
