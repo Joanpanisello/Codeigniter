941df2bfe668a2062aa334122aa03747054d4f6e
root <root@JPC.(none)>
2013-10-01 12:22:22 +0200

    css bootstrap aplicat

--- application/views/llistar.php
+++ application/views/llistar.php
@@ -2,13 +2,27 @@
 <html>
 <head>
 
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables.css">
- 
-<link rel="stylesheet" type="text/css" href="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/css/jquery.dataTables_themeroller.css">
+<!-- DataTables CSS -->
 
+<style type="text/css" title="currentStyle">
+	@import "demo_page.css"; 
+</style>
+
+<style type="text/css" title="currentStyle">
+	@import "/bootstrap/css/bootstrap.css"; 
+</style>
+
+<!-- jQuery  
+<link rel="stylesheet" type="text/css" href="http://www.datatables.net/release-datatables/media/css/demo_table.css">
+
+<link rel="stylesheet" type="text/css" href="http://www.datatables.net/release-datatables/media/css/demo_page.css">
+-->
 <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.8.2.min.js"></script>
  
+<!-- DataTables -->
 <script type="text/javascript" charset="utf8" src="http://ajax.aspnetcdn.com/ajax/jquery.dataTables/1.9.4/jquery.dataTables.min.js"></script>
+
+
 
 
 <script type="text/javascript">
@@ -20,9 +34,9 @@ $('#users_table').dataTable();
 
 </head>
 <body>	
-<h1 align=center>Llista d'Usuaris</h1>
+<h1 align=center><u>Llista d'Usuaris</u></h1>
 <br>
-<table cellpadding="0" cellspacing="0" border="0" id="users_table" class="display" width="100%">
+<table cellpadding="0" cellspacing="0" border="0" id="users_table" class="table table-striped" width="100%">
 <thead bgcolor=#819FF7>
 <tr>
 <th>Usuari</th>
